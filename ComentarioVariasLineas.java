9472f7fcc122f1ca7cece37fe6457c069d55b424
Sergio Aragón <saragonr91@gmail.com>
2021-07-06 12:38:45 +0200

    Subida archivos java

---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,11 @@
+
+public class ComentarioVariasLineas {
+	
+	public static void main (String[] args) {
+		for(int i = 0; i < 6; i++){
+			System.out.print(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+		}
+	}
+
+}
